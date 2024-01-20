c7490f1816aeabce84a9879286a8b20a05cb0662
Waterfox <waterfox80@gmail.com>
2024-01-20 22:08:03 +0100

    Add react-type-animation package and handle planet click event

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -1,36 +1,44 @@
 import React, { useEffect, useState } from 'react';
 
-interface PlanetProps {
-  color: string;
-  size: number;
-  rotationSpeed: number;
-  orbitRadius: number;
+export interface PlanetProps {
+    color: string;
+    size: number;
+    rotationSpeed: number;
+    orbitRadius: number;
+    onPlanetClick: () => void;
 }
 
-const Planet: React.FC<PlanetProps> = ({ color, size, rotationSpeed, orbitRadius }) => {
-  const [angle, setAngle] = useState(0);
+const Planet: React.FC<PlanetProps> = ({ color, size, rotationSpeed, orbitRadius, onPlanetClick }) => {
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setAngle((prevAngle) => (prevAngle + rotationSpeed) % 360);
-    }, 1); 
+    const [angle, setAngle] = useState(0);
 
-    return () => clearInterval(interval);
-  }, [rotationSpeed]);
-
-  const planetStyle: React.CSSProperties = {
-    backgroundColor: color,
-    width: `${size}px`,
-    height: `${size}px`,
-    position: 'absolute',
-    top: '50%',
-    left: '25%',
-    transformOrigin: 'center',
-    transform: `translate(-50%, -50%) rotate(${angle}deg) translate(${orbitRadius}px)`,
     
-};
 
-  return <div className="planet rounded-full" style={planetStyle}></div>;
+    const handleClick = () => {
+        onPlanetClick();
+      };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setAngle((prevAngle) => (prevAngle + rotationSpeed) % 360);
+        }, 1);
+
+        return () => clearInterval(interval);
+    }, [rotationSpeed]);
+
+    const planetStyle: React.CSSProperties = {
+        backgroundColor: color,
+        width: `${size}px`,
+        height: `${size}px`,
+        position: 'absolute',
+        top: '50%',
+        left: '25%',
+        transformOrigin: 'center',
+        transform: `translate(-50%, -50%) rotate(${angle}deg) translate(${orbitRadius}px)`,
+
+    };
+
+    return <div className="planet rounded-full" style={planetStyle} onClick={handleClick}></div>;
 };
 
 export default Planet;
--- src/pages/SolarSystem.tsx
+++ src/pages/SolarSystem.tsx
@@ -1,34 +1,56 @@
-import React from 'react';
-import Planet from '../components/Planet';
+import React, { useState, useEffect } from 'react';
+import Planet, { PlanetProps } from '../components/Planet'; 
 import projectData from '../data/projects.json';
+import { TypeAnimation } from 'react-type-animation';
 
-const SolarSystem: React.FC = () => {
-    return (
-        <div className="grid grid-cols-2">
-            <div className="col-span-1">
-                <div className="solar-system bg-gray-900 h-screen p-16">
-                    <div className="sun"></div>
-                    {Object.entries(projectData).map(([projectName, projectDetails], index) => (
-                        <Planet
-                            key={projectName}
-                            color={projectDetails.color}
-                            size={20 + index * 10}
-                            rotationSpeed={getRandomSpeed()}
-                            orbitRadius={90 + index * 50}
-                        />
-                    ))}
-                </div>
-            </div>
-            <div className="col-span-1 bg-gradient-to-t bg-gray-900 border-gray border-2 border-gray-800">
-                <p className="text-3xl text-gray-100 text-center font-bold font-mono font-bold mt-16">My Portfolio.</p>
-            </div>
+interface SolarSystemProps {}
+
+const SolarSystem: React.FC<SolarSystemProps> = () => {
+  const [selectedPlanet, setSelectedPlanet] = useState<string | null>(null);
+  const [selectedDescription , setSelectedDescription] = useState<string | null>(null);
+
+  const handlePlanetClick = (planetName: string) => {
+    console.log(`Clicked on ${planetName}`);
+    setSelectedPlanet(planetName);
+    setSelectedDescription(projectData[planetName as keyof typeof projectData].description);
+  };
+
+  return (
+    <div className="grid grid-cols-2">
+      <div className="col-span-1">
+        <div className="solar-system bg-gray-900 h-screen p-16">
+          <div className="sun"></div>
+          {Object.entries(projectData).map(([projectName, projectDetails], index) => (
+            <Planet
+              key={projectName}
+              color={projectDetails.color}
+              size={40 + index * 10}
+              rotationSpeed={getRandomSpeed()}
+              orbitRadius={150 + index * 50}
+              onPlanetClick={() => handlePlanetClick(projectName)}
+            />
+          ))}
         </div>
-    );
+      </div>
+      <div className="col-span-1 bg-gradient-to-t bg-gray-900 border-gray-800">
+        <div>
+          <p className="text-3xl text-gray-100 text-center font-bold font-mono font-bold mt-16">My Portfolio.</p>
+          <div className="info-box mt-16 mr-16">
+            <p id="planetName" className='text-gray-200'>
+                <u>PLANET</u> : {selectedPlanet ? selectedPlanet : "NONE"}
+            </p>
+            <p id="planetDescription" className='text-gray-200'>
+                <u>DESCRIPTION</u> : {selectedDescription ? selectedDescription : "???"}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-const getRandomSpeed = () => {
-    return (Math.random() / 5) + 0.1;
+const getRandomSpeed = (): number => {
+  return (Math.random() / 5) + 0.1;
 };
-
 
 export default SolarSystem;
